4146defb048f5b110587b0c3f8044dae3c357824
Benny Halevy <bhalevy@scylladb.com>
2019-02-04 17:00:18 +0200

    sstables: call remove_by_toc_name in dtor if marked_for_deletion
    No need to call delete_sstables which works on a list of sstable
    (by toc name).
    
    Also, add FIXME comment about not calling
    large_data_handler.maybe_delete_large_partitions_entry
    on this path.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -2856,9 +2856,6 @@ int sstable::compare_by_max_timestamp(const sstable& other) const {
     return (ts1 > ts2 ? 1 : (ts1 == ts2 ? 0 : -1));
 }
 
-future<>
-delete_sstables(std::vector<sstring> tocs);
-
 sstable::~sstable() {
     if (_index_file) {
         _index_file.close().handle_exception([save = _index_file, op = background_jobs().start()] (auto ep) {
@@ -2881,7 +2878,11 @@ sstable::~sstable() {
         // clean up unused sstables, and because we'll never reuse the same
         // generation number anyway.
         try {
-            delete_sstables({filename(component_type::TOC)}).handle_exception(
+            // FIXME: need to call large_data_handler.maybe_delete_large_partitions_entry
+            // POR to fix that is by passing large_data_handler upon construction and
+            // adding sstable::unlink, to be called here, that will update large_data_handler
+            // and delete the sstable.
+            remove_by_toc_name(toc_filename()).handle_exception(
                         [op = background_jobs().start()] (std::exception_ptr eptr) {
                             try {
                                 std::rethrow_exception(eptr);
