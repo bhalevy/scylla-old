219e0170ac416a245f7dda507a80a002c00553a9
Piotr Jastrzebski <piotr@scylladb.com>
2019-02-14 12:51:24 +0100

    storage_service: add _sstables_format field
    Signed-off-by: Piotr Jastrzebski <piotr@scylladb.com>

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -61,6 +61,7 @@
 #include "disk-error-handler.hh"
 #include "gms/feature.hh"
 #include <seastar/core/metrics_registration.hh>
+#include "sstables/version.hh"
 
 namespace cql_transport {
     class cql_server;
@@ -301,6 +302,8 @@ private:
     gms::feature _mc_sstable_feature;
     gms::feature _row_level_repair_feature;
     gms::feature _truncation_table;
+
+    sstables::sstable_version_types _sstables_format = sstables::sstable_version_types::ka;
 public:
     void enable_all_features();
 
